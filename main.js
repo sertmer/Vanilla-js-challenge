f069fba660fbd9d0316b563e13d9c6e0bf7a85f7
sertmer <scottertmer@gmail.com>
2019-12-12 10:11:51 -0700

    add func to enable submitguess button

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let challengerOne = document.querySelector('#name-one-input');
 let challengerTwo = document.querySelector('#name-two-input');
 let guessOne = document.querySelector('#guess-one-input');
 let guessTwo = document.querySelector('#guess-two-input');
+let submitGuessBtn = document.querySelector('#submit-guess-button')
 var randomNum
 
 window.onload = () => {
@@ -12,4 +13,11 @@ const generateRandomNumber = () => {
   let minNum = Math.ceil(1)
   let maxNum = Math.floor(100)
   randomNum = Math.floor(Math.random() * (maxNum - minNum)) + minNum
+}
+
+const enableSubmitGuessButton = () => {
+  if (challengerOne && guessOne && challengerTwo && guessTwo) {
+    console.log('enable submit', 'in here')
+    submitGuessBtn.disabled = false;
+  }
 }
